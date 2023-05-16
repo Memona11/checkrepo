27a794a89cca30a9daf5b1c5e79f47cf944c4478
Memona <miranimemona@gmail.com>
2023-05-16 12:31:43 +0500

    First commit

---
+++ eclipse_project/src/eclipse_project/Apply.java
@@ -0,0 +1,12 @@
+package eclipse_project;
+
+public class Apply {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      addition  ob =new addition();
+      System.out.println( ob.add(3, 6));
+      
+	}
+
+}
---
+++ eclipse_project/src/eclipse_project/addition.java
@@ -0,0 +1,9 @@
+package eclipse_project;
+
+public class addition {
+      int add(int a, int b) {
+    	int result= a+b;
+    	 return result ;
+    	 
+     }
+}
